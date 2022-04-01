bb8dcc0135c9b1b6ef6396f844021413d35c15a7
kevinkace <kevinacameron@hotmail.com>
2022-04-01 11:22:00 -0700

    v1

--- src/index.js
+++ src/index.js
@@ -4,7 +4,40 @@ import css from "./index.css";
 
 import "minireset.css";
 
-m.mount(document.getElementById("mount"), {
-    view : () =>
-        m("div", { class : css.component }, "component")
+const mount = document.getElementById("mount");
+const skills = [{
+    name: "skill 1",
+    coolDown: false,
+    coolDownDur: 4000
+}];
+
+m.mount(mount, {
+    view() {
+        return [
+            m("button", { onclick: () => { skills[0].coolDown = true; } }, "cooldown"),
+            m("button", { onclick: () => { skills[0].coolDown = false; } }, "reset"),
+            m("button", {
+                onclick: () => {
+                    const skill = skills[0];
+                    window.s = skill;
+
+                }
+            }, "buff"),
+            m("div.skills", skills.map(skill =>
+                m("div.skill",
+                    skill.coolDown ?
+                        m("div.coolDown", {
+                            oncreate(cdVnode) {
+                                skill.dom = cdVnode.dom;
+                            },
+                            style: {
+                                animationDuration: `${skill.coolDownDur}ms`
+                            }
+                        }) :
+                        null,
+                    m("div.name", skill.name)
+                )
+            ))
+        ];
+    }
 });
